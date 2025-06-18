25907665d7074890c658079d4e00fc18e7d3f627
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-18 18:13:48 +0000

    First Pattern problem done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,33 +13,6 @@ public class Main {
     }
   }
 
-  static void pat2(int n) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.print("* ");
-      }
-      System.out.println();
-    }
-  }
-
-  static void pat3(int n) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.print(j + 1);
-      }
-      System.out.println();
-    }
-  }
-
-  static void pat4(int n) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.print(i + 1);
-      }
-      System.out.println();
-    }
-  }
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     // pat1(n);
@@ -47,14 +20,10 @@ public class Main {
     for (int i = 0; i < t; i++) {
       int n;
       n = sc.nextInt();
-      pat4(n);
+      pat1(n);
     }
 
     sc.close();
   }
 
-  // @Test
-  // void addition() {
-  // assertEquals(2, 1 + 1);
-  // }
 }
