9625c74c5f5f2db3543181ba92d5c1e1501c03b6
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-18 19:44:41 +0000

    Add .gitignore and remove ignored files from repo

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-      pkgs.gh
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
