b73b5fdc736c0f48c4b5054dc2d001e761d13253
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-18 19:51:50 +0000

    Second Problem Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,17 @@ public class Main {
     }
   }
 
+  static void pat2(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+
+  
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     // pat1(n);
@@ -20,10 +31,14 @@ public class Main {
     for (int i = 0; i < t; i++) {
       int n;
       n = sc.nextInt();
-      pat1(n);
+      pat2(n);
     }
 
     sc.close();
   }
 
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
 }
