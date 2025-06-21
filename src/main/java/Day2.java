1891be0cf1c69a99f03bb14d98a2f848ecc8aa86
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-21 17:24:46 +0000

    add another method to solve number of digit problem via log10

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,17 +1,22 @@
 import java.util.*;
+import java.math.*;
 
 public class Day2 {
 
-  public static void count(int n) {
-    int c = 0;
-    int lastdigit;
-    while (n > 0) {
-      lastdigit = n % 10;
-      n = n / 10;
-      c++;
-    }
-    System.out.println("Number of digit are: " + c);
+  // public static void count(int n) {
+  //   int c = 0;
+  //   int lastdigit;
+  //   while (n > 0) {
+  //     lastdigit = n % 10;
+  //     n = n / 10;
+  //     c++;
+  //   }
+  //   System.out.println("Number of digit are: " + c);
 
+  // }
+  // add another method to solve number of digit problem via log10
+  static void count(int n){
+    System.out.println((int)Math.log10(n)+1);
   }
 
   public static void main(String[] args) {
