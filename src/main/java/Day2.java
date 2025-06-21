122154e36b39aba3b596702369f512bfa4c6b359
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-21 17:42:26 +0000

    probelm 2: Reverse a number

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -4,19 +4,33 @@ import java.math.*;
 public class Day2 {
 
   // public static void count(int n) {
-  //   int c = 0;
-  //   int lastdigit;
-  //   while (n > 0) {
-  //     lastdigit = n % 10;
-  //     n = n / 10;
-  //     c++;
-  //   }
-  //   System.out.println("Number of digit are: " + c);
+  // int c = 0;
+  // int lastdigit;
+  // while (n > 0) {
+  // lastdigit = n % 10;
+  // n = n / 10;
+  // c++;
+  // }
+  // System.out.println("Number of digit are: " + c);
 
   // }
   // add another method to solve number of digit problem via log10
-  static void count(int n){
-    System.out.println((int)Math.log10(n)+1);
+  // static void count(int n){
+  // System.out.println((int)Math.log10(n)+1);
+  // }
+
+  // probelm 2: Reverse a number
+  static void rev(int n) {
+    int fix = n;
+    int revNum = 0;
+    int lastDigit = 0;
+    while (n > 0) {
+      lastDigit = n % 10;
+      n = n / 10;
+      revNum = (revNum * 10) + lastDigit;
+    }
+    System.out.println("Original Number: " + fix);
+    System.out.println("Reverse Number: " + revNum);
   }
 
   public static void main(String[] args) {
@@ -25,7 +39,7 @@ public class Day2 {
     for (int i = 0; i < t; i++) {
       int n;
       n = sc.nextInt();
-      count(n);
+      rev(n);
     }
     sc.close();
   }
