bb59a01845a61085725bfbd36f463ed9aa65c6b9
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-22 17:52:01 +0000

    Day 3: problem 2: Print ALl Divisors with  O(√n + (no of factors * log(no of factors))

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -3,30 +3,51 @@ import java.util.*;
 public class Day3 {
   // Problem 1: Armstrong Number
   // static void armstrong(int n) {
-    
-  //   int original = n;
-  //   int sum = 0;
-  //   int lastDigit;
-  //   while (n > 0) {
-  //     lastDigit = n % 10;
-  //     sum = sum + (lastDigit * lastDigit * lastDigit);
-  //     n = n / 10;
-  //   }
-    
-  //   if (original == sum) {
-  //     System.out.println(original + " is an Armstrong Number.");
-  //   } else {
-  //     System.out.println(original + " is not an Armstrong Number.");
-  //   }
+
+  // int original = n;
+  // int sum = 0;
+  // int lastDigit;
+  // while (n > 0) {
+  // lastDigit = n % 10;
+  // sum = sum + (lastDigit * lastDigit * lastDigit);
+  // n = n / 10;
+  // }
+
+  // if (original == sum) {
+  // System.out.println(original + " is an Armstrong Number.");
+  // } else {
+  // System.out.println(original + " is not an Armstrong Number.");
+  // }
   // }
 
   // Problem 2: Print All Divisors
-  static void printAllDivisors(int n){
+  // static void printAllDivisors(int n){
+  // System.out.print(n + ": ");
+  // for(int i =1; i<=n; i++){
+  // if(n % i == 0){
+  // System.out.print(i+", ");
+  // }
+  // }
+  // System.out.println();
+  // }
+  // Problem 2: Print ALl Divisors with  O(√n + (no of factors * log(no of factors))
+  static void printAllDivisors(int n) {
     System.out.print(n + ": ");
-    for(int i =1; i<=n; i++){
-      if(n % i == 0){
-        System.out.print(i+", ");
-      } 
+    List<Integer> ls = new ArrayList<Integer>();
+    // O(sqrt(n))
+    for (int i = 1; i * i <= n; i++) {
+      if (n % i == 0) {
+        ls.add(i);
+        if ((n / i) != i) {
+          ls.add(n / i);
+        }
+      }
+    }
+    // O(no of factors * log(no of factors)): n is the number of factors
+    Collections.sort(ls);
+    // O(number of factors)
+    for (int l : ls) {
+      System.out.print(l + ", ");
     }
     System.out.println();
   }
