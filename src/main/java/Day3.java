684902fc1edb98ee5b0a3ac1e28fb54bf2c70464
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-22 17:25:18 +0000

    Day 3: problem 2: Print All Divisors

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -2,22 +2,33 @@ import java.util.*;
 
 public class Day3 {
   // Problem 1: Armstrong Number
-  static void armstrong(int n) {
+  // static void armstrong(int n) {
     
-    int original = n;
-    int sum = 0;
-    int lastDigit;
-    while (n > 0) {
-      lastDigit = n % 10;
-      sum = sum + (lastDigit * lastDigit * lastDigit);
-      n = n / 10;
-    }
+  //   int original = n;
+  //   int sum = 0;
+  //   int lastDigit;
+  //   while (n > 0) {
+  //     lastDigit = n % 10;
+  //     sum = sum + (lastDigit * lastDigit * lastDigit);
+  //     n = n / 10;
+  //   }
     
-    if (original == sum) {
-      System.out.println(original + " is an Armstrong Number.");
-    } else {
-      System.out.println(original + " is not an Armstrong Number.");
+  //   if (original == sum) {
+  //     System.out.println(original + " is an Armstrong Number.");
+  //   } else {
+  //     System.out.println(original + " is not an Armstrong Number.");
+  //   }
+  // }
+
+  // Problem 2: Print All Divisors
+  static void printAllDivisors(int n){
+    System.out.print(n + ": ");
+    for(int i =1; i<=n; i++){
+      if(n % i == 0){
+        System.out.print(i+", ");
+      } 
     }
+    System.out.println();
   }
 
   public static void main(String[] args) {
@@ -26,7 +37,7 @@ public class Day3 {
     for (int i = 0; i < t; i++) {
       int n;
       n = sc.nextInt();
-      armstrong(n);
+      printAllDivisors(n);
     }
     sc.close();
   }
