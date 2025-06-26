7ab9967ccb106baa908bce5eb8baa71b5d87f584
Mukund Jha <mukundjha204@gmail.com>
2025-06-26 09:49:22 +0000

    Problem 1: Print name N times using Recursion

--- src/main/java/Recursion.java
+++ src/main/java/Recursion.java
@@ -1,17 +1,30 @@
+import java.util.*;
+
 public class Recursion {
-   static int count = 1;
+   // static int count = 1;
 
-  // Started Recursion in Java with Striver
-  static void f() {
-    if(count == 4){
-      return;
-    }
-    System.out.println(count);
-    count++;
-    f();
-  }
+   // Started Recursion in Java with Striver
+   // static void f() {
+   // if(count == 4){
+   // return;
+   // }
+   // System.out.println(count);
+   // count++;
+   // f();
+   // }
 
-  public static void main(String[] args) {
-    f();
-  }
+   // Problem 1: Print name N times using Recursion
+   static void f(int i, int n) {
+      if (i > n) {
+         return;
+      }
+      System.out.println("Mukund");
+      f(i+1, n);
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      f(1, n);
+   }
 }
