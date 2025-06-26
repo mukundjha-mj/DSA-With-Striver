c0964c49a5357b32468a11bada451b96b7532b4b
mukundjha-mj <mukundjha204@gmail.com>
2025-06-26 10:49:43 +0000

    Problem 5: Print lineraly from N -> 1 (without using i - 1)

--- src/main/java/Recursion.java
+++ src/main/java/Recursion.java
@@ -40,17 +40,27 @@ public class Recursion {
    // }
 
    // Problem 4: Print lineraly from 1 to n (without using i+1)
+   // static void f(int i, int n) {
+   // if (i < 1) {
+   // return;
+   // }
+   // f(i - 1, n);
+   // System.out.println(i);
+   // }
+
+   // Problem 5: Print lineraly from N -> 1 (without using i - 1)
+
    static void f(int i, int n) {
-      if (i < 1) {
+      if (n < i) {
          return;
       }
-      f(i - 1, n);
+      f(i + 1, n);
       System.out.println(i);
    }
 
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int n = sc.nextInt();
-      f(n, n);
+      f(1, n);
    }
 }
