7ec0c332b805ffc15f20d0f72f530569f8c4984d
Mukund Jha <19338597-mukundjha-mj@users.noreply.replit.com>
2025-06-27 02:17:56 +0000

    Problem 1: Sum of first N Number

--- src/main/java/Recursion.java
+++ src/main/java/Recursion.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Recursion {
    // static int count = 1;
 
+   // DAY 1
    // Started Recursion in Java with Striver
    // static void f() {
    // if(count == 4){
@@ -49,18 +50,32 @@ public class Recursion {
    // }
 
    // Problem 5: Print lineraly from N -> 1 (without using i - 1)
+   // static void f(int i, int n) {
+   // if (n < i) {
+   // return;
+   // }
+   // f(i + 1, n);
+   // System.out.println(i);
+   // }
 
-   static void f(int i, int n) {
-      if (n < i) {
-         return;
+   // DAY 2
+
+   // Problem 1: Sum of first N Number
+   static int sum = 0;
+
+   static int sumFuncation(int i, int n) {
+      if (i > n) {
+         return 0;
       }
-      f(i + 1, n);
-      System.out.println(i);
+      sum = sum + i;
+      sumFuncation(i + 1, n);
+      return sum;
    }
 
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int n = sc.nextInt();
-      f(1, n);
+      int result = sumFuncation(1, n);
+      System.out.println(result);
    }
 }
