94cf66d28f9047d345cfb77baa51684e84747fb5
mukundjha-mj <mukundjha204@gmail.com>
2025-08-12 12:36:31 +0000

    Day 4: Problem 1: Bubble Sort done

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -15,6 +15,18 @@ public class Day4 {
     }
   }
 
+  public static void bubbleSort(int arr[], int n){
+    for(int i = n-1; i >=1; i--){
+      for(int j = 0; j<=i-1; j++){
+        if(arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  }
+
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int n;
@@ -23,7 +35,7 @@ public class Day4 {
     int arr[] = new int[6];
     for (int i = 0; i < n; i++)
       arr[i] = sc.nextInt();
-    selectionSort(arr, n);
+    bubbleSort(arr, n);
 
     for (int i = 0; i < n; i++) {
       System.out.print(arr[i] + " ");
