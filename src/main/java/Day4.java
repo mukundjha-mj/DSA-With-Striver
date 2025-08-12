05de9b60310ceff52fe8d0c5220b91c12c99acde
mukundjha-mj <mukundjha204@gmail.com>
2025-08-12 12:27:16 +0000

    Day 4: Problem 1: Selection Sort done

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,31 +1,32 @@
 import java.util.*;
-public class Day4{
-  public static void selectionSort(int arr[], int n){
-    for(int i=0; i<=n-2; i++){
-      int min = i; 
-      for(int j=i; j <= n - 1; j++){
-        if(arr[j] < arr[min]){
+
+public class Day4 {
+  public static void selectionSort(int arr[], int n) {
+    for (int i = 0; i <= n - 2; i++) {
+      int min = i;
+      for (int j = i; j <= n - 1; j++) {
+        if (arr[j] < arr[min]) {
           min = j;
         }
       }
-    int temp = arr[min];
-    arr[min] = arr[i];
-    arr[i] = temp;
+      int temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
     }
   }
 
-
-
-  public static void main(String args[]){
+  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    int n; ;
+    int n;
+    ;
     n = sc.nextInt();
     int arr[] = new int[6];
-    for(int i = 0; i < n; i ++) arr[i] = sc.nextInt();
+    for (int i = 0; i < n; i++)
+      arr[i] = sc.nextInt();
     selectionSort(arr, n);
 
-    for (int i = 0; i < n; i++){
-      System.out.print(arr[i]+ " ");
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
     }
   }
 }
